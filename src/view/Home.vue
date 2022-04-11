98243274c8af31bd9697b5a061d0343001982dd0
Manmeet Moudgill <manmeetmoudgill76@gmail.com>
2022-04-11 19:45:45 +0200

    Files Added

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -61,12 +61,15 @@
     >
       <button
         :disabled="this.$store.state.pageNumber == 1"
+        v-if="this.$store.state.pageNumber !== 1"
         v-on:click="previousPage"
         class="mt-4 py-2 px-4 text-white text-center rounded-md shadow-md bg-blue-600 hover:bg-blue-500 transition-all delay-150"
       >
         Previous
       </button>
       <button
+      v-if="!(this.$store.state.pageNumber + 1 >
+          Math.ceil(this.$store.state.totalNews / 20))"
         :disabled="
           this.$store.state.pageNumber + 1 >
           Math.ceil(this.$store.state.totalNews / 20)
