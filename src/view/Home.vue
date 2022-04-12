348155b8f9cbd071ddd67aaee9b93e61726637e6
Manmeet Moudgill <manmeetmoudgill76@gmail.com>
2022-04-12 19:26:42 +0200

    Files Added

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,91 +1,86 @@
 <template>
   <div class="container">
-    <div class="flex justify-between  flex-col sm:flex-row  sm:items-center">
+    <div class="flex justify-between flex-col sm:flex-row sm:items-center">
       <h2 class="text-5xl">News</h2>
-      {{this.$store.state.messageAlert}}
-
-      <form action="" class="flex flex-col  ">
-      
-
-        
+      <form action="" class="flex flex-col">
         <div class="">
-          <select v-model="selectedSource" @change="sourceChanged($event)"
-            class="  border  border-gray-300 px-4 py-1  text-sm font-medium text-gray-700 hover:bg-gray-50  focus:ring-2 focus:ring-offset-2   z-50  mt-2 w-full sm:w-56 rounded-md shadow-md bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-            
-          >
-              <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-              <option disabled value="">Filter by source</option>
-              <option  value="all">All</option>
-              <option
-                
-                v-for="source in this.$store.state.newsSources" :key="source"
-                class="text-gray-700 hover:bg-gray-200 block px-4 py-2 text-sm"
-                role="menuitem"
-                selected
-                tabindex="-1"
-                id="menu-item-0"
-                >{{source}}</option
-              >
-           
+          <select v-model="selectedSource" @change="sourceChanged($event)" class="
+              border border-gray-300
+              px-4
+              py-1
+              text-sm
+              font-medium
+              text-gray-700
+              hover:bg-gray-50
+              focus:ring-2 focus:ring-offset-2
+              z-50
+              mt-2
+              w-full
+              sm:w-56
+              rounded-md
+              shadow-md
+              bg-white
+              ring-1 ring-black ring-opacity-5
+              focus:outline-none
+            ">
+            <option disabled value="">Filter by source</option>
+            <option value="all">All</option>
+            <option v-for="source in this.$store.state.newsSources" :key="source"
+              class="text-gray-700 hover:bg-gray-200 block px-4 py-2 text-sm" role="menuitem" selected tabindex="-1"
+              id="menu-item-0">
+              {{ source }}
+            </option>
           </select>
         </div>
 
-        <div class="flex items-center mt-2  ">
-          
-          <input
-            type="checkbox"
-            v-on:change="OrderByTitleChanged"
-            v-model="OrderByTitle"
-            name="orderByTitle"
-            class="w-4 h-4 rounded-md shadow-md"
-            id="orderByTitle"
-          />
-          <label class="form-check-label   ml-2 inline-block text-gray-800" for="orderByTitle">Order by title</label>
+        <div class="flex items-center mt-2">
+          <input type="checkbox" v-on:change="OrderByTitleChanged" v-model="OrderByTitle" name="orderByTitle"
+            class="w-4 h-4 rounded-md shadow-md" id="orderByTitle" />
+          <label class="form-check-label ml-2 inline-block text-gray-800" for="orderByTitle">Order by title</label>
         </div>
-
-         
-
       </form>
     </div>
     <News :news="this.$store.state.news" />
 
-    <div
-      class="flex justify-center"
-      v-if="
-        this.$store.state.news.length === 0 &&
-        this.$store.state.isActive === false
-      "
-    >
+    <div class="flex justify-center" v-if="
+      this.$store.state.news.length === 0 &&
+      this.$store.state.isActive === false
+    ">
       <h2 class="text-2xl font-bold">Sorry there,no news found</h2>
     </div>
 
-  <div class="flex justify-center mb-3">
-    <div
-      v-if="this.$store.state.news.length !== 0"
-      class="flex justify-center items-center mt-2"
-    >
-    <img  
-        v-if="this.$store.state.pageNumber !== 1" class=" w-4 mr-2 hover:scale-105 transition-all delay-100 transform hover:-translate-x-2 cursor-pointer"
-        v-on:click="previousPage" src="../assets/arrow.png" alt="">
-    
-    
+    <div class="flex justify-center mb-3">
+      <div v-if="this.$store.state.news.length !== 0" class="flex justify-center items-center mt-2">
+        <img v-if="this.$store.state.pageNumber !== 1" class="
+            w-4
+            mr-2
+            hover:scale-105
+            transition-all
+            delay-100
+            transform
+            hover:-translate-x-2
+            cursor-pointer
+          " v-on:click="previousPage" src="../assets/arrow.png" alt="" />
 
-    <img   v-if="
+        <img v-if="
           this.$store.state.pageNumber + 1 ===
           Math.ceil(this.$store.state.totalNews / 20)
-        "
-    class="cursor-pointer w-4 ml-2 hover:scale-105 transition-all delay-100 transform hover:translate-x-2"
-        v-on:click="nextPage" src="../assets/right-arrow.png" alt="">
-    
-    
+        " class="
+            cursor-pointer
+            w-4
+            ml-2
+            hover:scale-105
+            transition-all
+            delay-100
+            transform
+            hover:translate-x-2
+          " v-on:click="nextPage" src="../assets/right-arrow.png" alt="" />
+      </div>
     </div>
-  </div>
-   
   </div>
 </template>
 
 <script>
-
 import News from "../components/News.vue";
 export default {
   name: "Home",
@@ -97,7 +92,7 @@ export default {
       OrderByTitle: "",
       pageNumber: 1,
       totalNews: null,
-      selectedSource:""
+      selectedSource: "",
     };
   },
   methods: {
@@ -114,19 +109,17 @@ export default {
       this.$store.dispatch("previous");
       window.scrollTo(0, 0);
     },
-    sourceChanged(event){
-     this.$store.dispatch("getNewBasedOnSource",{
-       source:event.target.value
-     })
-    }
+    sourceChanged(event) {
+      this.$store.dispatch("getNewBasedOnSource", {
+        source: event.target.value,
+      });
+    },
   },
   mounted() {
     this.$store.dispatch("getNews");
-    
-   
   },
-  created(){
-    this.totalNews=this.$store.state.totalNews;
-  }
+  created() {
+    this.totalNews = this.$store.state.totalNews;
+  },
 };
 </script>
